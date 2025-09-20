e296cd5893fc919e8186ba0f2baff5cdf9ffa691
Sanyu_Gloria22 <sanyugloria243@gmail.com>
2025-09-20 11:02:09 +0300

    I added new files, and deleted some files i transfered files from MW&F to here

--- Js/form.js
+++ Js/form.js
@@ -1,20 +1,40 @@
-// const form = document.getElementById("login-form");
-// const errorText = document.getElementById("error");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
 
-// form.addEventListener("submit", function(event) {
-//   event.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
 
-//   const emailAddress = document.getElementById("exampleInputEmail1").value;
-//   const password = document.getElementById("exampleInputPassword1").value;
-//   const role = document.getElementById("role").value;
+  
+    const customerName = form.customerName.value.trim();
+    const productType = form.productType.value.trim();
+    const productName = form.productName.value.trim();
+    const quantity = form.quantity.value.trim();
+    const unitPrice = form.unitPrice.value.trim();
+    const saleDate = form.saleDate.value;
+    const paymentType = form.paymentType.value;
+    const agentName = form.agentName.value.trim();
+    const transport = form.transport.checked;
 
-//   if (role === "admin" ) {
-//     window.location.href = "admin.html";
-//   }
+    if (!customerName) return alert("Please enter customer name.");
+    if (!productType) return alert("Please enter product type.");
+    if (!productName) return alert("Please enter product name.");
+    if (quantity <= 0) return alert("Quantity must be greater than 0.");
+    if (unitPrice <= 0) return alert("Price per unit must be greater than 0.");
+    if (!saleDate) return alert("Please select a sale date.");
+    if (!agentName) return alert("Please enter sales agent name.");
 
-//   else {
-//     errorText.textContent = "Invalid emailAddress or Password";
-//   }
+    let totalPrice = quantity * unitPrice;
+    if (transport) {
+      totalPrice *= 1.05; 
+    }
 
-// });
+    alert(
+      `Sale recorded!\n\nCustomer: ${customerName}\nProduct: ${productName}\nSales Agent Name: ${agentName}\nQuantity: ${quantity}\nTotal Price: ${totalPrice.toFixed(
+        2
+      )} UGX`
+    );
+
+    form.reset();
+  });
+});
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+// Dependencies
 const express = require("express");
 const path = express("path");
 
 
+//import routes
+
+
+//instantiations
 const app = express();
 const port = 3000;
 
+//configurations
 
+//setting up mongoDB configurations
+mongoose.connect(process.env.MONGODB_URI, {
+
+  //  useNewUrlParser: true,
+  //  useUnifiedTopology: true
+
+})
+.then(() => console.log(" Connected to MongoDB Atlas"))
+.catch(err => console.error(" MongoDB connection error:", err));
+
+
+//setting engine to pug
 
 app.get("/index", (req, res) =>{
   res.send('Welcome to Mayoundo Wood and Furniture');
